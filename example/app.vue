a419c64de2dbdabd22dc87e78316f88679086490
Funny <2048412928@qq.com>
2024-04-18 18:07:21 +0800

    backup

--- example/app.vue
+++ example/app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
     <h3>App</h3>
+    <dynamic-models ref="onRef" field="div"/>
+    <dynamic-models field="button" children="按钮" @click="onClick"/>
   </div>
 </template>
+
+<script setup lang="ts">
+import DynamicModels from 'dynamic-models/core';
+import { ref } from 'vue';
+
+const onRef = ref();
+
+function onClick() {
+  console.log(onRef.value);
+}
+</script>
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,29 +1,40 @@
-import { App, defineComponent } from 'vue';
+import { App, defineComponent, inject, PropType, shallowRef } from 'vue';
 
 export interface BaseField {
   /* 标识 */
   field: string; // field 标识渲染组件
   /* 显示 */
-  show: boolean;
+  show?: boolean;
   /* 属性 */
   prop?: string; // 为组件 v-model 添加
   /* 静态属性 */
-  attrs: Record<string, any>;
+  attrs?: Record<string, any>;
   /* 插槽值 */
-  slotData: Record<string, any>;
+  slotData?: Record<string, any>;
   /* 动态属性 */
-  dynamic: Record<string, string>;
+  dynamic?: Record<string, string>;
   /* 子节点 */
-  children: BaseField[] | BaseField | any;
+  children?: BaseField[] | BaseField | any;
   /* 插槽 */
-  slots: Record<string, BaseField['children']>;
+  slots?: Record<string, BaseField['children']>;
 }
 
 export const DynamicModels = defineComponent({
   name: 'DynamicModels',
-  props: {},
-  setup(props, { attrs, expose }: any) {
-    //
+  props: {
+    show: { type: Boolean as PropType<BaseField['show']>, default: () => true },
+    attrs: { type: Object as PropType<BaseField['attrs']>, default: () => ({}) },
+    slots: { type: Object as PropType<BaseField['slots']>, default: () => ({}) },
+    prop: { type: String as PropType<BaseField['prop']>, default: () => undefined },
+    slotData: { type: Object as PropType<BaseField['slotData']>, default: () => ([]) },
+    dynamic: { type: [Object, Array] as PropType<BaseField['dynamic']>, default: () => ([]) },
+    field: { type: String as PropType<BaseField['field']>, default: () => (''), required: true },
+    children: { type: [Array, Object, String, Number] as PropType<BaseField['children']>, default: () => undefined },
+  },
+  setup(props, ctx: any) {
+    const modules = inject('dynamic-modules', shallowRef({}));
+    console.log(props);
+    return () => null;
   },
 });
 
